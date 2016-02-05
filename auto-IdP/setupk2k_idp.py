97a642ab1ea81e1fe30d53bb73139c94beed0d55
Kristi Nikolla <knikolla@bu.edu>
2016-02-05 16:52:40 -0500

    Port number changed

--- auto-IdP/setupk2k_idp.py
+++ auto-IdP/setupk2k_idp.py
@@ -11,14 +11,14 @@ try:
     OS_PROJECT_ID = os.environ['OS_PROJECT_ID']
     OS_USER_ID = os.environ['OS_USER_ID']
     OS_PASSWORD = os.environ['OS_PASSWORD']
-except KeyError as e:  
+except KeyError as e:
     raise SystemExit('%s environment variable not set.' % e)
 
 def client_for_admin_user():
     auth = v3.Password(auth_url=OS_AUTH_URL,
                        user_id=OS_USER_ID,
                        password=OS_PASSWORD,
-                       project_id=OS_PROJECT_ID)
+                       project_id=OS_PROJECT_ID
     session = ksc_session.Session(auth=auth)
     return keystone_v3.Client(session=session)
 
@@ -28,18 +28,18 @@ print "print user list to verify client object"
 print client.users.list()
 
 SP_url="http://" + sp_ip + ":5000/Shibboleth.sso/SAML2/ECP"
-AUTH_url="http://" + sp_ip + ":5000/v3/OS-FEDERATION/identity_providers/keystone-idp/protocols/saml2/auth"
+AUTH_url="http://" + sp_ip + ":35357/v3/OS-FEDERATION/identity_providers/keystone-idp/protocols/saml2/auth"
 
 
-def create_sp(client, sp_id, sp_url, auth_url):  
+def create_sp(client, sp_id, sp_url, auth_url):
         sp_ref = {'id': sp_id,
                   'sp_url': sp_url,
                   'auth_url': auth_url,
                   'enabled': True}
         return client.federation.service_providers.create(**sp_ref)
 
-print('\nCreate SP')  
-create_sp(client,  
+print('\nCreate SP')
+create_sp(client,
           'keystone-sp',
           SP_url,
           AUTH_url)
