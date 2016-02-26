6781bfa92e5c44f87c51eb7612f9682dc22b47ee
Kristi Nikolla <knikolla@bu.edu>
2016-02-26 11:48:11 -0500

    Changed auth_uri to auth_url in nova.conf

--- auto-IdP/modify_novaconf.py
+++ auto-IdP/modify_novaconf.py
@@ -4,7 +4,7 @@ import configparser
 config = configparser.ConfigParser()
 config.read('/etc/nova/nova.conf')
 
-config['cinder']['auth_uri'] = config['keystone_authtoken']['auth_url']
+config['cinder']['auth_url'] = config['keystone_authtoken']['auth_url']
 
 with open('/etc/nova/nova.conf', 'w') as configfile:
     config.write(configfile)
